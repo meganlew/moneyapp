60fd5086da7441ae99b6d5437479a9f2431057e7
meganlew <meganlew@Megans-Air-2.attlocal.net>
2021-11-16 18:13:42 -0800

    working on react

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+// third party, found in package.json
+import { Link , Switch, Route} from 'react-router-dom'; 
+
+import Home from './Home'; //relative path
+import SignUp from './SignUp';
+import Feed from './Feed';
+
 
 function App() {
   return (
     <div>
-      Add your code here!
+      <ul>
+        <li><Link to = "/">Home</Link></li>
+        <li><Link to = "/sign-up">Sign Up</Link></li>
+        <li><Link to = "/Feed">Feed</Link></li>
+      </ul>
+    <Switch>
+     <Route path = "/Feed" component ={Feed} />
+      <Route path = "/sign-up" component ={SignUp} />
+      <Route path = "/">
+        <Home/>
+      </Route>
+    </Switch>
     </div>
   );
 }
---
+++ front-end/src/Home.js
@@ -0,0 +1,65 @@
+// Step 1 import react
+import React from 'react';
+
+// Step 2 create a component function that returns an element
+const Home = () => {
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const[result, setResult] = React.useState(null);
+
+    const handleSignUp = () => {
+       console.log('User has sent money !', username, password);
+       const body = {
+         username: username,
+         password: password,
+       };
+       // make an http call to java
+       const settings = {
+           method: 'post',
+           body: JSON.stringify(body),
+       }
+       fetch('/api/sign-up', settings)//built in
+       .then(res => res.json())
+       .then(data => {
+          console.log(data)
+          setResult(data); // what spark responds
+       })
+       .catch(console.log); //async try/catch
+    };
+
+    if(result !==null && result.isSuccess){
+        return(
+            <div>
+                Welcome {username}!
+            </div>
+        );
+    }
+    return(
+        <div>
+            <h1>Home</h1>
+            <div>
+                <h3>Sender</h3>
+                <input 
+                value = {username} 
+                onChange = {e => setUsername(e.target.value)}
+                />
+            </div>
+            <div>
+                <h3>Reciever</h3>
+                <input 
+                type = "password" 
+                value= {password}
+                onChange = {e => setPassword(e.target.value)}
+                />
+            </div>
+            <div>
+                <button onClick = {handleSignUp}>Send Amount</button>
+            </div>
+            {(result !==null && result.isSuccess) && <div>{result.message}</div>}
+        </div>
+    );
+};
+
+// Step 3 
+export default Home; // equivalent to "public" in java
+
